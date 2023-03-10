37d026967c650f4912252bfd380db8c39b84c98f
Abbes74 <126820966+Abbes74@users.noreply.github.com>
2023-03-10 10:19:39 +0100

    Add files via upload

---
+++ wafx_res/generated/wafxSearchFullText_fr.js
@@ -0,0 +1,11 @@
+ { 
+"map_targets":[
+ {  "title":"Page sans titre" , "display_url":"index.html" , "url":"index.html" } 
+],
+"map_words":["","bouton","mon"]
+,
+"index_words":[ [0 ] 
+, [0 ] 
+, [0 ] 
+]
+ } 
